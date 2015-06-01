e094e1471f46abc21e62b9514f094198dce0e66e
Qijin Zhou <qijin.zhou@outlook.com>
2015-05-31 20:20:03 -0700

    Add trie_set.

--- trie.hpp
+++ trie.hpp
@@ -7,6 +7,74 @@ namespace q
 {
 
 
+template<class TKey>
+class trie_set
+{
+	template<class TNodeKey>
+	struct trie_node
+	{
+		TNodeKey key;
+		std::vector<std::unique_ptr<trie_node>> children;
+	};
+
+public:
+	void clear()
+	{
+		_root.children.clear();
+		_size = 0;
+	}
+
+	template<class TKeyIter>
+	void emplace(TKeyIter begin, TKeyIter end)
+	{
+		auto pNode = &_root;
+		for (auto itr = begin; itr < end; ++itr)
+		{
+			auto&& key = *itr;
+			auto& children = pNode->children;
+			auto itrChild = std::lower_bound(children.begin(), children.end(), key, [](auto&& spNode, auto&& theKey){ return spNode->key < theKey; });
+			if (itrChild == children.end() || (*itrChild)->key != key)
+			{
+				itrChild = children.emplace(itrChild, std::make_unique<trie_node<TKey>>());
+				(*itrChild)->key = key;
+			}
+			pNode = itrChild->get();
+		}
+		++_size;
+	}
+
+	template<class TKeyIter>
+	bool find(TKeyIter begin, TKeyIter end)
+	{
+		auto pNode = &_root;
+		for (auto itr = begin; itr < end; ++itr)
+		{
+			auto&& key = *itr;
+			auto& children = pNode->children;
+			auto itrChild = std::lower_bound(children.begin(), children.end(), key, [](auto&& spNode, auto&& theKey){ return spNode->key < theKey; });
+			if (itrChild == children.end() || (*itrChild)->key != key)
+				return false;
+			pNode = itrChild->get();
+		}
+		return true;
+	}
+
+	bool empty() const
+	{
+		return _size == 0;
+	}
+
+	size_t size() const
+	{
+		return _size;
+	}
+
+private:
+	trie_node<TKey> _root;
+	size_t _size = 0;
+};
+
+
 template<class TKey, class TValue>
 class trie_map
 {
---
+++ trie_set.test.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+
+#include "trie.hpp"
+
+TEST(trie_set, clear)
+{
+	q::trie_set<char> t;
+	std::string key("hello");
+	t.emplace(key.begin(), key.end());
+
+	t.clear();
+	EXPECT_FALSE(t.find(key.begin(), key.end()));
+	EXPECT_EQ(0u, t.size());
+}
+
+TEST(trie_set, emplace)
+{
+	q::trie_set<char> t;
+	std::string key("hello");
+	t.emplace(key.begin(), key.end());
+
+	std::string key2("world");
+	t.emplace(key2.begin(), key2.end());
+}
+
+TEST(trie_set, find)
+{
+	q::trie_set<char> t;
+	std::string key("hello");
+	t.emplace(key.begin(), key.end());
+
+	std::string find1("hello");
+	EXPECT_TRUE(t.find(find1.begin(), find1.end()));
+
+	std::string find2("world");
+	EXPECT_FALSE(t.find(find2.begin(), find2.end()));
+}
+
+TEST(trie_set, empty)
+{
+	q::trie_set<char> t;
+	EXPECT_TRUE(t.empty());
+
+	std::string key("hello");
+	t.emplace(key.begin(), key.end());
+	EXPECT_FALSE(t.empty());
+}
+
+TEST(trie_set, size)
+{
+	q::trie_set<char> t;
+	EXPECT_EQ(0u, t.size());
+
+	std::string key("hello");
+	t.emplace(key.begin(), key.end());
+	EXPECT_EQ(1u, t.size());
+}
